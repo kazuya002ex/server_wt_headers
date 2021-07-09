2cdf1774c8d402f8f99ddfb5af6ee42ff1e4eede
kazuya002ex <hara.ex002@gmail.com>
2021-07-09 18:56:34 +0900

    feat: テーブル追加、admin#topの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :top, only: %i(index login)
+  end
+
+  root 'admin/top#index'
 end
