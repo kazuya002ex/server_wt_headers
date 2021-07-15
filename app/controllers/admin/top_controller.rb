e8a04084f211a2e30d733c1135d961153f70e54f
kazuya002ex <chiharu.event2@gmail.com>
2021-07-15 10:15:50 +0900

    feat: routes追加

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,4 +1,7 @@
 class Admin::TopController < ApplicationController
   def index
   end
+
+  def login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'admin/top#index'
+
   namespace :admin do
     resources :top, only: %i(index login)
+    resources :header_contents, only: %i(index show create update destroy)
+    resources :tags, only: %i(index show create update destroy)
+    resources :genres, only: %i(index show create update destroy)
   end
-
-  root 'admin/top#index'
 end
